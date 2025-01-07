131369c5e869ae21d6bd8cf8d3daf9fdbd21a87a
Sahiiil1406 <supersen@gmail.com>
2025-01-07 18:50:11 +0530

    build+proxy

---
+++ buildServer/api/views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import requests
+import json
+import docker
+import os
+
+@csrf_exempt
+def run(request):
+    if request.method == "POST":
+        try:
+            # Parse the JSON request body
+            body = json.loads(request.body)
+            github_url = body.get("githubUrl", None)
+            docker_user = body.get("dockerUser", None)
+            docker_pass = body.get("dockerPass", None)
+            image_name = body.get("imageName", None)
+            cmd = body.get("cmd", None)
+            env_content = body.get("env_content", "")
+        
+
+            # Validate required parameters
+            if not all([github_url, docker_user, docker_pass, image_name]):
+                return JsonResponse({
+                    "status": "error",
+                    "message": "Missing required parameters (githubUrl, dockerUser, dockerPass, imageName)."
+                })
+
+            # Environment variables
+            env_vars = {
+                "GIT_URL": github_url,
+                "DOCKER_USERNAME": docker_user,
+                "DOCKER_PASSWORD": docker_pass,
+                "IMAGE_NAME": image_name,
+                "CMD":cmd,
+                "ENV_CONTENT": env_content
+            }
+            # Docker volume bindings
+            repo_name = github_url.split("/")[-1].replace(".git", "")
+            
+
+            # Initialize Docker client
+            client = docker.from_env()
+
+            # Run the container
+            
+           # docker run   -e GIT_URL=https://github.com/Sahiiil1406/ServerHealthChecker.git   -e DOCKER_USERNAME=sahil1406   -e DOCKER_PASSWORD=dckr_pat_xtvZyHE9hxT7nYSXW0cPACJlibc   -e IMAGE_NAME=serverhealthchecker   -v /var/run/docker.sock:/var/run/docker.sock   --privileged   final
+            # os.system(f"docker run -d -e GIT_URL={github_url} -e DOCKER_USERNAME={docker_user} -e DOCKER_PASSWORD={docker_pass} -e IMAGE_NAME={image_name} -v /var/run/docker.sock:/var/run/docker.sock --privileged final")
+            #get logs
+
+            container = client.containers.run(
+                    image="final_deploy",  # Replace 'final' with your image name
+                    detach=True,
+                    environment=env_vars,
+                    volumes={
+                        "/var/run/docker.sock": {"bind": "/var/run/docker.sock", "mode": "rw"}
+                    },
+                    privileged=True
+                )
+
+            logs = container.logs(stream=True)
+            logs_output = ""
+            for log in logs:
+               logs_output += log.decode('utf-8')
+
+            # Stop and remove the container and remove image
+            
+            container.stop()     
+            container.remove() 
+
+            #hit a URL to show success:url:http://172.25.96.1:8001/api/webhook ,request type:POST,req.body:{id,image_name,image_port}
+            payload = {
+                "id": container.id,
+                "image_name": image_name,
+                "image_port": "3000"
+            }
+            print("sahil")
+            try:
+                webhook_url = "http://localhost:8001/api/webhook/"
+                response = requests.post(webhook_url, json=payload)
+                print(response.json())
+            except Exception as e:
+                print(f"Failed to hit webhook URL: {str(e)}")
+
+            return JsonResponse({
+                "status": "success",
+                "message": "Container executed successfully.",
+                "containerId": container.id,
+                "containerLogs": logs_output.strip()
+            })
+
+        except Exception as e:
+            return JsonResponse({
+                "status": "error",
+                "message": "Failed to execute container.",
+                "error": str(e),
+            })
+
+    return JsonResponse({
+        "status": "error",
+        "message": "Invalid request method. Use POST."
+    })
