656e7de228a1e6836f8ca3622680683647348f47
Yash Kumar Singh <yashkumarsingh@Yashs-MacBook-Air.local>
2026-06-28 23:49:40 +0530

    vercel fix3

--- buildServer/buildServer/settings.py
+++ buildServer/buildServer/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'buildServer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
